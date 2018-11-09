d0f1db74cff4364e9e7b33fc5fa10c800381ac8b
Martin <mts13@hotmail.fr>
2018-11-09 01:30:34 +0100

    Mashallah

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,17 @@
 class MoviesController < ApplicationController
   def search
-    title = params[:movie]
-    @movie = SearchMovie.new.search(title)
+    if params[:movie].blank?
+      flash[:info] = "Aucun film trouvé !"
+      redirect_to '/'
+    else
+      title = params[:movie]
+      @movie = SearchMovie.new.search(title)
+    end
+  end
+
+  helper_method :credit
+
+  def credit(id)
+    @director = SearchMovie.new.find_credit(id)
   end
 end
--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -4,4 +4,18 @@ class SearchMovie
     Tmdb::Api.language("fr")
     george = Tmdb::Movie.find(title)
   end
+
+  def find_credit(id)
+    Tmdb::Api.key("da6f4749d2a814b4626fd0944eb2ca36")
+    Tmdb::Api.language("fr")
+    clients = Tmdb::Movie.credits(id)
+    #director = clients["crew"].map
+    #clients.select{|key, hash| hash["job"] == "Director" }
+    if Tmdb::Movie.credits(clients['id'])['crew'].blank? == false
+      director = Tmdb::Movie.credits(clients['id'])['crew'].select { |x| x['job'] == 'Director' }[0]['name']
+    else
+      director = "Inconnu"
+    end
+    return director
+  end
 end
